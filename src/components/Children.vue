81f60b54044620fb808dad6e6ffe10d0f927a842
Jongkeun Kim <kjkandrea@gmail.com>
2020-09-02 11:10:49 +0900

    property(prop) 넘기기

---
+++ src/components/Children.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{parentMessage}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Children extends Vue {
+  @Prop() public parentMessage?: string;
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,22 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <Message />
+    <Children :parentMessage="message" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Message from '@/components/Message.vue';
+import Children from '@/components/Children.vue';
 
 @Component({
   components: {
     Message,
+    Children,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  public message: string = 'Hello, Prop!';
+}
 </script>

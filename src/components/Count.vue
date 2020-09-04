0f12431693115c86aa6ebf48d9a6e33c5f959ef7
Jongkeun Kim <kjkandrea@gmail.com>
2020-09-04 14:58:50 +0900

    Vuex 시범운행

---
+++ src/components/Count.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    {{ $store.getters.count }}
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator'
+
+export default class Count extends Vue {}
+</script>
---
+++ src/views/VuexUse.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Count />
+    <button @click="decrease()">감소</button>
+    <button @click="increase()">증가</button>
+  </div>
+</template>
+
+<script lang="ts">
+
+import { Component, Vue } from 'vue-property-decorator';
+import Count from '@/components/Count.vue'
+
+@Component({
+  components: {
+    Count,
+  },
+})
+
+export default class VuexUse extends Vue {
+  increase() {
+    this.$store.dispatch('increase')
+  }
+
+  decrease() {
+    this.$store.dispatch('decrease')
+  }
+}
+
+</script>
